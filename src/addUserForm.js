9cbf506d7e2a365c50344d07eef1c6e9d2d38d01
mj <jdsk88@gmail.com>
2020-02-18 23:28:46 +0100

    user-add-form

---
+++ src/addUserForm.js
@@ -0,0 +1,89 @@
+export class addUserForm {
+  constructor() {
+    this.form = document.querySelector("form.user-add-form");
+    this.firstName = document.querySelector("input#fname");
+    this.lastName = document.querySelector("input#lname");
+    this.email = document.querySelector("input#email");
+    this.phone = document.querySelector("input#tel");
+    this.userName = document.querySelector("input#uname");
+    this.passwd = document.querySelector("input#passwd");
+    this.validationContener = document.querySelector(".validation");
+    this.sendform();
+  }
+  //send new user data to server
+  sendform() {
+    console.log("form");
+    this.form.addEventListener("submit", e => {
+      e.preventDefault();
+      // check if typed user already exist
+      console.log("submit");
+      if (this.validationForm()) {
+        fetch(
+          `http://localhost:3000/users?user_name=${this.userName.value}&password=${this.passwd.value}&first_name=${this.firstName.value}&last_name=${this.lastName.value}&e_mail=${this.email.value}&tel=${this.phone.value}`,
+          {}
+        )
+          .then(response => {
+            return response.json();
+          })
+          .then(response => {
+            // if response
+            if (response.length == 0) {
+              this.insertCustomer();
+              this.form.reset();
+              this.validationContener.style.display = "none";
+            } else {
+              console.log("Uzytkownik o danych: ");
+              console.log(this.userName.value);
+              console.log(this.passwd.value);
+              console.log(this.firstName.value);
+              console.log(this.lastName.value);
+              console.log(this.email.value);
+              console.log(this.phone.value);
+              console.log("jest zarejestrowany w bazie ");
+              this.validationContener.style.display = "block";
+              this.validationContener.innerText = "Uzytkownik juz istnieje!";
+            }
+          });
+      } else {
+        this.validationContener.style.display = "block";
+        this.validationContener.innerText = "Wypełnij wszystkie pola!";
+      }
+    });
+  }
+
+  insertCustomer() {
+    const time = new Date();
+    fetch("http://localhost:3000/users", {
+      headers: { "Content-Type": "application/json; charset=utf-8" },
+      method: "POST",
+      body: JSON.stringify({
+        user_name: `${this.userName.value}`,
+        password: `${this.passwd.value}`,
+        first_name: `${this.firstName.value}`,
+        last_name: `${this.lastName.value}`,
+        e_mail: `${this.email.value}`,
+        tel: `${this.phone.value}`,
+        dateJoin: `${time}`
+      })
+    });
+    console.log("Uzytkowniki o danych : ");
+    console.log(this.userName.value);
+    console.log(this.passwd.value);
+    console.log(this.firstName.value);
+    console.log(this.lastName.value);
+    console.log(this.email.value);
+    console.log(this.phone.value);
+    console.log("został zarejsetwowany w bazie ");
+  }
+  validationForm() {
+    if (
+      this.firstName.value.length > 0 &&
+      this.lastName.value.length > 0 &&
+      this.email.value.length > 0 &&
+      this.phone.value > 0
+    ) {
+     return true;
+    }
+    return false;
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { addUserForm } from './addUserForm.js'
+
+window.addEventListener("load", event => {
+let form = new addUserForm();
+});
+
